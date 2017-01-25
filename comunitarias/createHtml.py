7b404f0aaaa0f146ed76f54ee9de0d04958907dc
Daniel Bellomo <dbellomo@gmail.com>
2017-01-25 10:48:11 -0300

    titulo index

--- comunitarias/createHtml.py
+++ comunitarias/createHtml.py
@@ -32,7 +32,7 @@ html_body = """<!DOCTYPE html>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>| Radios</title>
+        <title>| Radios Comunitarias</title>
         
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="./bootstrap-3.3.7-dist/css/bootstrap.min.css">
